e0d00e02f1560d07240ed0b5d17ff19d0f857913
urlzengbin <yy282371715@sina.cn>
2018-10-08 14:31:43 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,58 +1,62 @@
-$(function () {
-
-  var cobweb = new Cobweb().init();
-
-  function animate() {
-    requestAnimationFrame(animate);
-
-    cobweb.update();
+function loadData(url,type,success) {
+  var xmlhttp;
+  if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+  }else {// code for IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
   }
-
-  animate();
-  // return;
-  //添加 项目集
-  $.ajax({
-    url: 'data/works.json',
-    dataType: "json",
-    type: "get",
-    success: function (data) {
-      var projectArr = data;
-      for (var i = 0; i < projectArr.length; i++) {
-
-        var name = projectArr[i]["name"];
-        var brand = projectArr[i]["brand"];
-        var pro_name = projectArr[i]["pro_name"];
-        var time = projectArr[i]["time"];
-        var desc = projectArr[i]["desc"];
-        var link = (projectArr[i]["link"] && projectArr[i]["link"] != "")
-          ? projectArr[i]["link"]
-          : "javascript:void(0);";
-
-        var img_url = "http://h5n.180china.com/game/pro/" + name + "_" + time + ".jpg";
-        var codeimg_url = "http://h5n.180china.com/game/pro/" + name + "_" + time + "_code.png";
-        var mask_ele = (projectArr[i]["code"] === 0)
-          ? ""
-          : ("<div class='item-mask'><img class='item-codeimg' src='" + codeimg_url + "' alt=''/></div>");
-
-        var item = "<li class='col-6 col-sm-4 col-xl-3'>" +
-          "<div class='item-view'>" +
-          "<a href='" + link + "' target='_blank'>" +
-          "<div class='item-head'>" +
-          "<img class='item-img' src='" + img_url + "'/>"
-          + mask_ele +
-          "</div>" +
-          "<h2 class='item-brand'>" + brand + "</h2>" +
-          "<div class='item-proname'>" + pro_name + "</div>" +
-          "</a>" +
-          "<div class='item-desc'>" + desc + "</div>" +
-          "</div>" +
-          "</li>";
-
-        $("#projectView").append(item);
-      }
-    },
-    error: function (error) {
-
+  xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+      if(success)success(xmlhttp.responseText)
     }
-  });
+  }
+  xmlhttp.open(type,url,true);
+  xmlhttp.send();
+}
+
+//添加 项目集
+loadData("data/works.json","GET",function(data){
+  var html='';
+  var projectArr = JSON.parse(data);
+  for (var i = 0; i < projectArr.length; i++) {
+
+    var name = projectArr[i]["name"];
+    var brand = projectArr[i]["brand"];
+    var pro_name = projectArr[i]["pro_name"];
+    var time = projectArr[i]["time"];
+    var desc = projectArr[i]["desc"];
+    var link = (projectArr[i]["link"] && projectArr[i]["link"] != "")
+      ? projectArr[i]["link"]
+      : "javascript:void(0);";
+
+    var img_url = "http://h5n.180china.com/game/pro/" + name + "_" + time + ".jpg";
+    var codeimg_url = "http://h5n.180china.com/game/pro/" + name + "_" + time + "_code.png";
+    var mask_ele = (projectArr[i]["code"] === 0)
+      ? ""
+      : ("<div class='item-mask'><img class='item-codeimg' src='" + codeimg_url + "' alt=''/></div>");
+
+    html += "<li class='col-6 col-sm-4 col-xl-3'>" +
+      "<div class='item-view'>" +
+      "<a href='" + link + "' target='_blank'>" +
+      "<div class='item-head'>" +
+      "<img class='item-img' src='" + img_url + "'/>"
+      + mask_ele +
+      "</div>" +
+      "<h2 class='item-brand'>" + brand + "</h2>" +
+      "<div class='item-proname'>" + pro_name + "</div>" +
+      "</a>" +
+      "<div class='item-desc'>" + desc + "</div>" +
+      "</div>" +
+      "</li>";
+  }
+  document.getElementById('projectView').innerHTML=html;
 });
+
+var cobweb = new Cobweb().init();
+function animate() {
+  requestAnimationFrame(animate);
+
+  cobweb.update();
+}
+animate();
--- js/particle/Cobweb.js
+++ js/particle/Cobweb.js
@@ -3,112 +3,112 @@
  */
 
 function Cobweb(){
-    var windowWidth=document.documentElement.clientWidth;
-    var windowHeight=document.documentElement.clientHeight;
-    var scale=(windowWidth/1200)>1?1:(windowWidth/1200);
+  var windowWidth=document.documentElement.clientWidth;
+  var windowHeight=document.documentElement.clientHeight;
+  var scale=(windowWidth/1200)>1?1:(windowWidth/1200);
 
-    var canvas=document.createElement("canvas");
-    document.body.appendChild(canvas);
-    canvas.width=windowWidth;
-    canvas.height=windowHeight;
-    canvas.id="cobweb";
-    canvas.style.position="fixed";
-    canvas.style.top="0";
-    canvas.style.left="0";
-    canvas.style.zIndex=0;
+  var canvas=document.createElement("canvas");
+  document.body.appendChild(canvas);
+  canvas.width=windowWidth;
+  canvas.height=windowHeight;
+  canvas.id="cobweb";
+  canvas.style.position="fixed";
+  canvas.style.top="0";
+  canvas.style.left="0";
+  canvas.style.zIndex=0;
 
-    var ctx=canvas.getContext("2d");
+  var ctx=canvas.getContext("2d");
 
-    var pn=50*scale,
-        particles=[],
-        minDistance=150*scale,
-        maxDistance=minDistance*2;
+  var pn=50*scale,
+    particles=[],
+    minDistance=150*scale,
+    maxDistance=minDistance*2;
 
-    this.init=function(){
+  this.init=function(){
 
-        for(var i=0;i<pn;i++){
-            particles.push({
-                x:Math.random()*windowWidth,
-                y:Math.random()*windowHeight,
-                vx:0,
-                vy:0,
-                r:Math.random()*10+1,
-                circleFillStyle:(function(){
-                    var R=parseInt(Math.random()*255);
-                    var G=parseInt(Math.random()*255);
-                    var B=parseInt(Math.random()*255);
-                    var A=Math.random();
-                    //return "rgba("+R+","+G+","+B+","+A+")";
-                    var rgb=200;
-                    var a=0.3;
-                    return "rgba("+rgb+","+rgb+","+rgb+","+a+")";
-                }())
-            });
-        }
-
-        return this;
+    for(var i=0;i<pn;i++){
+      particles.push({
+        x:Math.random()*windowWidth,
+        y:Math.random()*windowHeight,
+        vx:0,
+        vy:0,
+        r:Math.random()*10+1,
+        circleFillStyle:(function(){
+          var R=parseInt(Math.random()*255);
+          var G=parseInt(Math.random()*255);
+          var B=parseInt(Math.random()*255);
+          var A=Math.random();
+          //return "rgba("+R+","+G+","+B+","+A+")";
+          var rgb=200;
+          var a=0.3;
+          return "rgba("+rgb+","+rgb+","+rgb+","+a+")";
+        }())
+      });
     }
 
-    this.update=function(){
-        ctx.save();
-        ctx.clearRect(0,0,canvas.width,canvas.height);
+    return this;
+  }
 
-        var v=0.2;
+  this.update=function(){
+    ctx.save();
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 
-        for(i=0;i<pn;i++){
-            for(var j=i+1;j<pn;j++){
-                var p1=particles[i];
-                var p2=particles[j];
-                var dis=calculateDistance(p1,p2);
+    var v=0.2;
 
-                if(dis<maxDistance){
-                    //ctx.globalAlpha = dis > minDistance ? (maxDistance - dis) / (maxDistance - minDistance) : 1;
-                    ctx.beginPath();
-                    ctx.moveTo(p1.x,p1.y);
-                    ctx.lineTo(p2.x,p2.y);
-                    var alpha=dis > minDistance ? (maxDistance - dis) / (maxDistance - minDistance) : 1;
-                    var rgb=240;
-                    ctx.strokeStyle='rgba('+rgb+','+rgb+','+rgb+','+alpha+')';
-                    ctx.closePath();
-                    ctx.stroke();
-                }
-            }
+    for(i=0;i<pn;i++){
+      for(var j=i+1;j<pn;j++){
+        var p1=particles[i];
+        var p2=particles[j];
+        var dis=calculateDistance(p1,p2);
+
+        if(dis<maxDistance){
+          //ctx.globalAlpha = dis > minDistance ? (maxDistance - dis) / (maxDistance - minDistance) : 1;
+          ctx.beginPath();
+          ctx.moveTo(p1.x,p1.y);
+          ctx.lineTo(p2.x,p2.y);
+          var alpha=dis > minDistance ? (maxDistance - dis) / (maxDistance - minDistance) : 1;
+          var rgb=240;
+          ctx.strokeStyle='rgba('+rgb+','+rgb+','+rgb+','+alpha+')';
+          ctx.closePath();
+          ctx.stroke();
         }
-
-        for(var i=0;i<pn;i++){
-            var particle=particles[i];
-            ctx.beginPath();
-            ctx.arc(particle.x,particle.y,particle.r,0,2*Math.PI,true);
-
-            //ctx.strokeStyle=particle.circleFillStyle;
-            ctx.fillStyle=particle.circleFillStyle;
-            ctx.closePath();
-            ctx.fill();
-            //ctx.stroke();
-
-            particle.x+=particle.vx;
-            if(particle.x<0)
-                particle.x=windowWidth;
-            else if(particle.x>windowWidth)
-                particle.x=0;
-
-            particle.y+=particle.vy;
-            if(particle.y<0)
-                particle.y=windowHeight;
-            else if(particle.y>windowHeight)
-                particle.y=0;
-
-            particle.vx+= v * (Math.random() - 0.5) - 0.02* particle.vx;
-            particle.vy+= v * (Math.random() - 0.5) - 0.02* particle.vy;
-        }
-
-        ctx.restore();
+      }
     }
 
+    for(var i=0;i<pn;i++){
+      var particle=particles[i];
+      ctx.beginPath();
+      ctx.arc(particle.x,particle.y,particle.r,0,2*Math.PI,true);
 
-    function calculateDistance(p1,p2){
-        var x=Math.abs(p1.x-p2.x);
-        var y=Math.abs(p1.y-p2.y);
-        return Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+      //ctx.strokeStyle=particle.circleFillStyle;
+      ctx.fillStyle=particle.circleFillStyle;
+      ctx.closePath();
+      ctx.fill();
+      //ctx.stroke();
+
+      particle.x+=particle.vx;
+      if(particle.x<0)
+        particle.x=windowWidth;
+      else if(particle.x>windowWidth)
+        particle.x=0;
+
+      particle.y+=particle.vy;
+      if(particle.y<0)
+        particle.y=windowHeight;
+      else if(particle.y>windowHeight)
+        particle.y=0;
+
+      particle.vx+= v * (Math.random() - 0.5) - 0.02* particle.vx;
+      particle.vy+= v * (Math.random() - 0.5) - 0.02* particle.vy;
     }
+
+    ctx.restore();
+  }
+
+
+  function calculateDistance(p1,p2){
+    var x=Math.abs(p1.x-p2.x);
+    var y=Math.abs(p1.y-p2.y);
+    return Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+  }
 }
