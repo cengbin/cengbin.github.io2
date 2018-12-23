44b39f3694118f25d2138f8762d2cccf7ea1b799
urlzengbin <yy282371715@sina.cn>
2018-12-23 19:17:48 +0800

    update

--- js/main.js
+++ js/main.js
@@ -17,46 +17,29 @@ function loadData(url,type,success) {
 
 //添加 项目集
 loadData("data/works.json","GET",function(data){
-  var html='';
-  var projectArr = JSON.parse(data);
-  for (var i = 0; i < projectArr.length; i++) {
+  var items = JSON.parse(data);
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i]
 
-    var name = projectArr[i]["name"];
-    var brand = projectArr[i]["brand"];
-    var pro_name = projectArr[i]["pro_name"];
-    var time = projectArr[i]["time"];
-    var desc = projectArr[i]["desc"];
-    var link = (projectArr[i]["link"] && projectArr[i]["link"] != "")
-      ? projectArr[i]["link"]
-      : "javascript:void(0);";
-
-    var img_url = "http://h5n.180china.com/game/pro/" + name + "_" + time + ".jpg";
-    var codeimg_url = "http://h5n.180china.com/game/pro/" + name + "_" + time + "_code.png";
-    var mask_ele = (projectArr[i]["code"] === 0)
-      ? ""
-      : ("<div class='item-mask'><img class='item-codeimg' src='" + codeimg_url + "' alt=''/></div>");
-
-    html += "<li class='col-6 col-sm-4 col-xl-3'>" +
-      "<div class='item-view'>" +
-      "<a href='" + link + "' target='_blank'>" +
-      "<div class='item-head'>" +
-      "<img class='item-img' src='" + img_url + "'/>"
-      + mask_ele +
-      "</div>" +
-      "<h2 class='item-brand'>" + brand + "</h2>" +
-      "<div class='item-proname'>" + pro_name + "</div>" +
-      "</a>" +
-      "<div class='item-desc'>" + desc + "</div>" +
-      "</div>" +
-      "</li>";
+    if(!item.link)item.link = "javascript:void(0);"
+    item.thumbnailUrl = "http://h5n.180china.com/game/pro/" + item.name + "_" + item.time + ".jpg";
+    item.codeUrl = (item.codeImg === 0)?null:"http://h5n.180china.com/game/pro/" + item.name + "_" + item.time + "_code.png";
   }
+
+  var item_template = document.getElementById('itemTemplte').innerHTML
+  var renderFun = mito(item_template)
+  var html = renderFun({
+    items:items
+  })
   document.getElementById('projectView').innerHTML=html;
+
+  var cobweb = new Cobweb().init();
+  function animate() {
+    requestAnimationFrame(animate);
+
+    cobweb.update();
+  }
+  animate();
 });
 
-var cobweb = new Cobweb().init();
-function animate() {
-  requestAnimationFrame(animate);
 
-  cobweb.update();
-}
-animate();
